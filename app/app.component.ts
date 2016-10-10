9b5398c8bb4eb573eccf8023cb02fc22e66eca8a
pimato <thomaspulber91@gmail.com>
2016-10-10 18:21:30 +0200

    npm package angular2-storage-sync added changed systemjs to include the new papge imported package Forms

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { StorageSync, StorageStrategy } from 'angular2-storage-sync';
+
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+  selector: 'my-app',
+  template:`
+    <input type="checkbox"  [(ngModel)]="remember"> Remember me
+    <button (click)="setItems()">Set items</button>
+<ul>
+  <li *ngFor="let item of items">
+  {{item.id}}
+</li>
+</ul>`
 })
-export class AppComponent { }
+export class AppComponent {
+  @StorageSync('rememberMe') remember: boolean = false;
+
+  @StorageSync(null, StorageStrategy.Session) items: Array<Object> = [];
+
+  setItems() {
+    // this is always sync with your storage
+    this.items = [{id: 1}, {id: 2}];
+  }
+}
--- systemjs.config.js
+++ systemjs.config.js
@@ -27,6 +27,7 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      'angular2-storage-sync': 'npm:angular2-storage-sync'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -38,6 +39,10 @@
         defaultExtension: 'js'
       },
       'angular-in-memory-web-api': {
+        main: './index.js',
+        defaultExtension: 'js'
+      },
+      'angular2-storage-sync': {
         main: './index.js',
         defaultExtension: 'js'
       }
